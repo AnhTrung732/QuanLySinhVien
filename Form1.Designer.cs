eb645968c89c3adead90e447daa08ea5701a0919
AnhTrung732 <20520332@gm.uit.edu.vn>
2022-10-18 16:34:08 +0700

    Lap trinh truc quan

---
+++ Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace FileSinhVien
+{
+    partial class SinhVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMaSV = new System.Windows.Forms.TextBox();
+            this.mssv_label = new System.Windows.Forms.Label();
+            this.ten_label = new System.Windows.Forms.Label();
+            this.txtTenSV = new System.Windows.Forms.TextBox();
+            this.malop_label = new System.Windows.Forms.Label();
+            this.txtLop = new System.Windows.Forms.TextBox();
+            this.dtb_label = new System.Windows.Forms.Label();
+            this.txtDiem = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.dataGridSinhVien = new System.Windows.Forms.DataGridView();
+            this.colMaSV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDiem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colLop = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnHuy = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridSinhVien)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtMaSV
+            // 
+            this.txtMaSV.Location = new System.Drawing.Point(132, 21);
+            this.txtMaSV.Name = "txtMaSV";
+            this.txtMaSV.Size = new System.Drawing.Size(199, 22);
+            this.txtMaSV.TabIndex = 0;
+            // 
+            // mssv_label
+            // 
+            this.mssv_label.AutoSize = true;
+            this.mssv_label.Location = new System.Drawing.Point(59, 21);
+            this.mssv_label.Name = "mssv_label";
+            this.mssv_label.Size = new System.Drawing.Size(45, 16);
+            this.mssv_label.TabIndex = 1;
+            this.mssv_label.Text = "MSSV";
+            // 
+            // ten_label
+            // 
+            this.ten_label.AutoSize = true;
+            this.ten_label.Location = new System.Drawing.Point(47, 64);
+            this.ten_label.Name = "ten_label";
+            this.ten_label.Size = new System.Drawing.Size(70, 16);
+            this.ten_label.TabIndex = 2;
+            this.ten_label.Text = "Họ và Tên";
+            // 
+            // txtTenSV
+            // 
+            this.txtTenSV.Location = new System.Drawing.Point(132, 64);
+            this.txtTenSV.Name = "txtTenSV";
+            this.txtTenSV.Size = new System.Drawing.Size(199, 22);
+            this.txtTenSV.TabIndex = 3;
+            // 
+            // malop_label
+            // 
+            this.malop_label.AutoSize = true;
+            this.malop_label.Location = new System.Drawing.Point(378, 70);
+            this.malop_label.Name = "malop_label";
+            this.malop_label.Size = new System.Drawing.Size(52, 16);
+            this.malop_label.TabIndex = 4;
+            this.malop_label.Text = "Mã Lớp";
+            // 
+            // txtLop
+            // 
+            this.txtLop.Location = new System.Drawing.Point(493, 67);
+            this.txtLop.Name = "txtLop";
+            this.txtLop.Size = new System.Drawing.Size(161, 22);
+            this.txtLop.TabIndex = 5;
+            // 
+            // dtb_label
+            // 
+            this.dtb_label.AutoSize = true;
+            this.dtb_label.Location = new System.Drawing.Point(378, 27);
+            this.dtb_label.Name = "dtb_label";
+            this.dtb_label.Size = new System.Drawing.Size(98, 16);
+            this.dtb_label.TabIndex = 6;
+            this.dtb_label.Text = "Điểm trung bình";
+            // 
+            // txtDiem
+            // 
+            this.txtDiem.Location = new System.Drawing.Point(493, 15);
+            this.txtDiem.Name = "txtDiem";
+            this.txtDiem.Size = new System.Drawing.Size(161, 22);
+            this.txtDiem.TabIndex = 7;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(88, 108);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(68, 29);
+            this.btnThem.TabIndex = 8;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // dataGridSinhVien
+            // 
+            this.dataGridSinhVien.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridSinhVien.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colMaSV,
+            this.colTen,
+            this.colDiem,
+            this.colLop});
+            this.dataGridSinhVien.Location = new System.Drawing.Point(-4, 191);
+            this.dataGridSinhVien.Name = "dataGridSinhVien";
+            this.dataGridSinhVien.RowHeadersWidth = 51;
+            this.dataGridSinhVien.RowTemplate.Height = 24;
+            this.dataGridSinhVien.Size = new System.Drawing.Size(753, 265);
+            this.dataGridSinhVien.TabIndex = 9;
+            this.dataGridSinhVien.SelectionChanged += new System.EventHandler(this.dataGridSinhVien_SelectionChanged);
+            // 
+            // colMaSV
+            // 
+            this.colMaSV.DataPropertyName = "MaSV";
+            this.colMaSV.HeaderText = "Mã Sinh Viên";
+            this.colMaSV.MinimumWidth = 6;
+            this.colMaSV.Name = "colMaSV";
+            this.colMaSV.Width = 125;
+            // 
+            // colTen
+            // 
+            this.colTen.DataPropertyName = "TenSV";
+            this.colTen.HeaderText = "Tên Sinh Viên";
+            this.colTen.MinimumWidth = 6;
+            this.colTen.Name = "colTen";
+            this.colTen.Width = 150;
+            // 
+            // colDiem
+            // 
+            this.colDiem.DataPropertyName = "Diem";
+            this.colDiem.HeaderText = "Điểm sinh viên";
+            this.colDiem.MinimumWidth = 6;
+            this.colDiem.Name = "colDiem";
+            this.colDiem.Width = 125;
+            // 
+            // colLop
+            // 
+            this.colLop.DataPropertyName = "Lop";
+            this.colLop.HeaderText = "Lớp";
+            this.colLop.MinimumWidth = 6;
+            this.colLop.Name = "colLop";
+            this.colLop.Width = 150;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(172, 108);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(68, 29);
+            this.btnSua.TabIndex = 10;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(263, 108);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(68, 29);
+            this.btnXoa.TabIndex = 11;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Location = new System.Drawing.Point(438, 108);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(68, 29);
+            this.btnLuu.TabIndex = 12;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Location = new System.Drawing.Point(529, 108);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(68, 29);
+            this.btnHuy.TabIndex = 13;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            // 
+            // SinhVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 454);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.dataGridSinhVien);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtDiem);
+            this.Controls.Add(this.dtb_label);
+            this.Controls.Add(this.txtLop);
+            this.Controls.Add(this.malop_label);
+            this.Controls.Add(this.txtTenSV);
+            this.Controls.Add(this.ten_label);
+            this.Controls.Add(this.mssv_label);
+            this.Controls.Add(this.txtMaSV);
+            this.Name = "SinhVien";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.SinhVien_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridSinhVien)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMaSV;
+        private System.Windows.Forms.Label mssv_label;
+        private System.Windows.Forms.Label ten_label;
+        private System.Windows.Forms.TextBox txtTenSV;
+        private System.Windows.Forms.Label malop_label;
+        private System.Windows.Forms.TextBox txtLop;
+        private System.Windows.Forms.Label dtb_label;
+        private System.Windows.Forms.TextBox txtDiem;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.DataGridView dataGridSinhVien;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMaSV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDiem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSinhVien
+{
+    public partial class SinhVien : Form
+    {
+        string flag;
+        DataTable dtSV;
+        int index;
+        string filePath = @"D:\\study\\C#_project\\FileSinhVien\\FileSinhVien\\SinhVien.txt";
+        public SinhVien()
+        {
+            InitializeComponent();
+        }
+        public DataTable createTable()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("MaSV");
+            dt.Columns.Add("TenSV");
+            dt.Columns.Add("Diem");
+            dt.Columns.Add("Lop");
+            return dt;
+        }
+        public DataTable ConvertToDataTable(string filePath)
+        {   
+            DataTable dtSample = createTable();
+            Console.WriteLine(dtSample.Columns.Count); 
+
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+            Console.WriteLine(lines[0]);
+
+            foreach (string line in lines)
+            {
+                var cols = line.Split('|');
+                Console.WriteLine(cols[0]);
+
+                DataRow dr = dtSample.NewRow();
+                for (int cIndex = 0; cIndex < dtSample.Columns.Count; cIndex++)
+                {
+                    dr[cIndex] = cols[cIndex];
+                }
+                dtSample.Rows.Add(dr);
+            }
+            return dtSample;
+        }
+        private void SinhVien_Load(object sender, EventArgs e)
+        {
+            LockControl();
+            //dtSV = createTable();
+            dtSV = ConvertToDataTable(filePath);
+            dataGridSinhVien.DataSource = dtSV;
+            dataGridSinhVien.RefreshEdit();
+        }
+
+        public void LockControl()
+        {
+            btnThem.Enabled = true;
+            btnXoa.Enabled = true;
+            btnSua.Enabled = true;
+            btnLuu.Enabled = false;
+            btnHuy.Enabled = false;
+
+            txtMaSV.ReadOnly = true;
+            txtDiem.ReadOnly = true;
+            txtLop.ReadOnly = true;
+            txtTenSV.ReadOnly = true;
+
+            btnThem.Focus();
+        }
+        public void UnlockControl()
+        {
+            btnThem.Enabled = false;
+            btnXoa.Enabled = false;
+            btnSua.Enabled = false;
+            btnHuy.Enabled = true;
+            btnLuu.Enabled = true;
+
+            txtMaSV.ReadOnly= false;
+            txtTenSV.ReadOnly = false;
+            txtDiem.ReadOnly= false;
+            txtLop.ReadOnly = false;
+
+            txtMaSV.Focus();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            UnlockControl();
+            flag = "add";
+
+            txtMaSV.Text = "";
+            txtTenSV.Text = "";
+            txtDiem.Text = "";
+            txtLop.Text = "";
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            UnlockControl();
+            flag = "edit";
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (flag == "add")
+            {
+                if (checkData())
+                {
+                    dtSV.Rows.Add(txtMaSV.Text, txtTenSV.Text, txtDiem.Text, txtLop.Text);
+                    dataGridSinhVien.DataSource = dtSV;
+                    dataGridSinhVien.RefreshEdit();
+                    Console.WriteLine(dtSV);
+
+                    StreamWriter w = new StreamWriter(filePath, true);
+                    foreach (DataRow row in dtSV.Rows)
+                    {
+                        object[] array = row.ItemArray;
+                        for (int i = 0; i < array.Length - 1; i++)
+                        {
+                            w.Write(array[i].ToString() + " | ");
+                        }
+                        w.WriteLine(array[array.Length - 1].ToString());
+                    }
+                    w.Close();
+                }
+            }    
+            else if(flag == "edit")
+            {
+                dtSV.Rows[index][0] = txtMaSV.Text;
+                dtSV.Rows[index][1] = txtTenSV.Text;
+                dtSV.Rows[index][2] = txtDiem.Text;
+                dtSV.Rows[index][3] = txtLop.Text;
+                dataGridSinhVien.DataSource = dtSV;
+                dataGridSinhVien.RefreshEdit();
+                StreamWriter w = new StreamWriter(filePath);
+                foreach (DataRow row in dtSV.Rows)
+                {
+                    object[] array = row.ItemArray;
+                    for (int i = 0; i < array.Length - 1; i++)
+                    {
+                        w.Write(array[i].ToString() + " | ");
+                    }
+                    w.WriteLine(array[array.Length - 1].ToString());
+                }
+                w.Close();
+            }    
+            LockControl();
+        }
+        public bool checkData()
+        {
+            if(string.IsNullOrWhiteSpace(txtMaSV.Text))
+            {
+                MessageBox.Show("Bạn chưa nhập mã sinh viên","Thông báo",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                txtMaSV.Focus();
+                return false;
+            }
+            if(string.IsNullOrWhiteSpace(txtTenSV.Text))
+            {
+                MessageBox.Show("Bạn chưa nhập tên sinh viên", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtTenSV.Focus();
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtDiem.Text))
+            {
+                MessageBox.Show("Bạn chưa nhập điểm", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtDiem.Focus();
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtLop.Text))
+            {
+                MessageBox.Show("Bạn chưa nhập lớp sinh viên", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtLop.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        private void dataGridSinhVien_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridSinhVien.CurrentCell != null)
+            {
+                index = dataGridSinhVien.CurrentCell.RowIndex;
+                DataTable dt = (DataTable)dataGridSinhVien.DataSource;
+                if (dt != null)
+                {
+                    txtMaSV.Text = dataGridSinhVien.Rows[index].Cells[0].Value.ToString();
+                    txtTenSV.Text = dataGridSinhVien.Rows[index].Cells[1].Value.ToString();
+                    txtDiem.Text = dataGridSinhVien.Rows[index].Cells[2].Value.ToString();
+                    txtLop.Text = dataGridSinhVien.Rows[index].Cells[3].Value.ToString();
+                }
+            }
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn xóa sinh viên này?","Cảnh báo",MessageBoxButtons.YesNo,MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                dtSV.Rows.RemoveAt(index);
+                dataGridSinhVien.DataSource = dtSV;
+                dataGridSinhVien.RefreshEdit();
+                StreamWriter w = new StreamWriter(filePath);
+                foreach (DataRow row in dtSV.Rows)
+                {
+                    object[] array = row.ItemArray;
+                    for (int i = 0; i < array.Length - 1; i++)
+                    {
+                        w.Write(array[i].ToString() + " | ");
+                    }
+                    w.WriteLine(array[array.Length - 1].ToString());
+                }
+                w.Close();
+            }
+        }
+    }
+}
